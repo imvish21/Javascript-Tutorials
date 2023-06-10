aff080e2f40dcef728a37f2fdcdc82b2e0987985
Vishal Kumar <92950796+imvish21@users.noreply.github.com>
2023-06-10 14:23:03 +0530

    objects in js

---
+++ primitive_datatype.js
@@ -0,0 +1,20 @@
+//primitive data types and objects
+ //object is non premitive data type in js
+ //7 types of data types in js
+ //1.Null 2.Number 3.Symbol 4.String 5.Boolean 6.Bigint 7.Undefined
+ let a=null
+ let b=45
+ let c="vishal"
+ let d=Symbol("I am symbol")
+ let e=true
+ let f=BigInt("454")+BigInt("30")
+ let g=undefined
+ console.log(a,b,c,d,e,f,g)
+ //object
+ const item={
+    "Harry":true,
+    "Vishal":456,
+    452:"solo",
+    "Rohan":undefined
+ }
+ console.log(item["Rohan"])
