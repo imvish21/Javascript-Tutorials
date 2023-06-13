6973241da531d9891b01930c8bba788e33ed1c9d
Vishal Kumar <imvgupta21@gmail.com>
2023-06-13 11:12:48 +0530

    for loop

---
+++ Javascript Codes/for_loops.js
@@ -0,0 +1,21 @@
+//for-in loop
+let obj={
+    shiva:55,
+    vishal:20,
+    hello:15
+}
+for(let a in obj){
+    console.log("Marks of "+a+" is "+obj[a])
+}
+
+//for-of loop.  (used with arrays and strings)
+for(let b of "vishal")  
+{
+    console.log(b)
+}
+//output-v
+       //i
+       //s
+       //h
+       //a
+       //l
