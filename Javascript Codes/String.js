276366ff4ef5e055656fffe4b369a5ac97539d2c
Vishal Kumar <imvgupta21@gmail.com>
2023-06-15 13:51:09 +0530

    string

---
+++ Javascript Codes/String.js
@@ -0,0 +1,12 @@
+let name="Vishal"
+console.log(name.length)
+//template literals
+let boy1="Pramod"
+let boy2='Nikhil'
+let sentence=`${boy1} is a friend of ${boy2}`
+console.log(sentence)
+//escape sequence characters
+//1. \'
+let a="bana\'na"
+console.log(a)
+//2. \n,\r,\t ,\"
