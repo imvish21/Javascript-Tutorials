fb608125c6292bcd3e4c581df1383fbbb994f32f
Vishal Kumar <imvgupta21@gmail.com>
2023-06-22 19:11:14 +0530

    Element Nav

---
+++ Javascript Codes/Element Only Navigation/script.js
@@ -0,0 +1,7 @@
+const changeBgRed = () => {
+   document.body.firstElementChild.style.color = "red"
+}
+
+/*let b=document.body
+console.log("First Child of b is:",b.firstChild)
+console.log("First Element Child of b is:",b.firstElementChild)*/
