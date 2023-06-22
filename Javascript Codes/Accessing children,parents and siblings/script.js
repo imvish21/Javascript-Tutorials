bd76c2a291327598a1f88e9db88cb5bb5fe62e64
Vishal Kumar <imvgupta21@gmail.com>
2023-06-22 14:39:58 +0530

    accessing nodes

---
+++ Javascript Codes/Accessing children,parents and siblings/script.js
@@ -0,0 +1,10 @@
+/*console.log(document.body.firstChild)
+console.log(document.body.lastChild)
+console.log(document.body.firstElementChild)
+console.log(document.body.lastElementChild)
+console.log(document.body.childNodes)*/
+console.log(document.body.firstChild)
+a=document.body.firstChild
+console.log(a.parentNode)
+console.log(a.parentElement)
+console.log(a.firstChild.nextSibling)
