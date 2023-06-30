d3aadd071dd9b8aa8e5b03a4b0cea6e250c91134
Vishal Kumar <imvgupta21@gmail.com>
2023-06-30 21:07:56 +0530

    inner and outer HTML

---
+++ Javascript Codes/innerHTML,outerHTML/script.js
@@ -0,0 +1,8 @@
+console.log(document.getElementsByTagName("span")[0])
+console.dir(document.getElementsByTagName("span")[0])
+//first.outerHTML="<b><div>Hey,i am</div></b>"
+console.log(document.body.firstChild.data) //hello world
+console.log(document.body.firstChild.nodeValue) //hello world (a text node's value)
+console.log(document.body.textContent)
+/*$0.hidden=false*/
+first.hidden=false
