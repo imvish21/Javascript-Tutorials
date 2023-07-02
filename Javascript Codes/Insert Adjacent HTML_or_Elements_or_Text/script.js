1427cd07d1a594ee15681687e9868419213f9d94
Vishal Kumar <imvgupta21@gmail.com>
2023-07-02 15:00:07 +0530

    insert adjacent html

---
+++ Javascript Codes/Insert Adjacent HTML_or_Elements_or_Text/script.js
@@ -0,0 +1,9 @@
+/*first.insertAdjacentHTML('beforebegin','<div class="test">Before begin</div>')
+first.insertAdjacentHTML('afterbegin','<div class="test">After begin</div>')
+first.insertAdjacentHTML('beforeend','<div class="test">Before end</div>')
+first.insertAdjacentHTML('afterend','<div class="test">After end</div>')
+first.insertAdjacentElement('afterend','<div class="test">After end element</div>')*/
+
+//remove node
+let first=document.getElementById("first")
+first.remove()
