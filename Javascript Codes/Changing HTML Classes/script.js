5b50d02b59272ced5ddb9d8ce226d3ea0443fb4a
Vishal Kumar <imvgupta21@gmail.com>
2023-07-02 15:52:38 +0530

    insert adjacent html

---
+++ Javascript Codes/Changing HTML Classes/script.js
@@ -0,0 +1,13 @@
+//className
+/*first.className="red text-black"
+
+//classList
+console.log(first.classList)
+console.log(first.classList[0])
+first.classList.remove("red")
+first.classList.add("yellow")
+//console.log(first.classList.toggle("yellow"))
+first.classList.toggle("red")  //adds red
+first.classList.toggle("yellow")  //removes yellow
+console.log(first.classList.toggle("red"))*/
+console.log(first.classList.contains("yellow")) //true
