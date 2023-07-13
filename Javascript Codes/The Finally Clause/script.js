f3eadbc57b063e0c69fe9d0714d3bdc9f9039372
Vishal Kumar <imvgupta21@gmail.com>
2023-07-13 14:12:45 +0530

    finally clause

---
+++ Javascript Codes/The Finally Clause/script.js
@@ -0,0 +1,12 @@
+try{
+  let a=0
+  console.log(b)
+  console.log("Program success")
+}
+catch(error){
+  console.log("This is an error")
+  console.log(p)
+}
+finally{
+  console.log("I am a good boy")
+}
