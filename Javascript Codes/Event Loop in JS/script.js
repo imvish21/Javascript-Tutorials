b1c8e54c7b46d6b308ad211d7fb6abe310469e22
Vishal Kumar <imvgupta21@gmail.com>
2023-07-22 15:33:04 +0530

    event loop

---
+++ Javascript Codes/Event Loop in JS/script.js
@@ -0,0 +1,11 @@
+
+console.log("Hi!");
+
+setTimeout(function timeout() {
+    console.log("Click the button5!");
+}, 5000);
+setTimeout(function timeout() {
+    console.log("Click the button4!");
+}, 4000);
+
+console.log("Welcome to loupe.");
